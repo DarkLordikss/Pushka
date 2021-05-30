76fd298e11f5da6330f32bcdf59f058a415acabc
Dark_Lordss <75681846+DarkLordikss@users.noreply.github.com>
2021-05-31 02:46:21 +0700

    Only fast post now

--- Pushka.py
+++ Pushka.py
@@ -1,14 +1,11 @@
 import grequests
+import requests
 import random
 import time
 
 n = int(input('Requests - '))
 i = 0
 count = 0
-int_count = 1
-need_inter = str(input('Need interval(y/n) - '))
-if need_inter == 'y':
-    inter = int(input('Requests per second - '))
 
 ts = int(time.time())
 strs = []
@@ -33,19 +30,13 @@ while i < n:
     soil1 = random.randint(1314, 1514)
     soil2 = random.randint(1314, 1514)
     soil3 = random.randint(1314, 1514)
-    id = random.randint(0, 80)
+    id = str(random.randint(0, 80))
+    id = id.zfill(4)
     ver = random.randint(1, 5)
 
-    if id < 10:
-        id = '0'+str(id)
+    strs.append('{'+'"t": {0}, "hm": {1}, "pr": {2}, "t0": {3}, "t1": {4}, "t2": {5}, "t3": {6}, "t4": {7}, "t5": {8}, "t6": {9}, "t7": {10}, "t8": {11}, "Uext": {12}, "Upow": {13}, "soil1": {14}, "soil2": {15}, "soil3": {16}, "stationID": "{17}", "device_type": "Z", "firmware_version": {18}, "time": {19}'.format(t, hm, pr, t0, t1, t2, t3, t4, t5, t6, t7, t8, uext, upow, soil1, soil2, soil3, id, ver, ts)+'}')
 
-    strs.append('{'+'"t": {0}, "hm": {1}, "pr": {2}, "t0": {3}, "t1": {4}, "t2": {5}, "t3": {6}, "t4": {7}, "t5": {8}, "t6": {9}, "t7": {10}, "t8": {11}, "Uext": {12}, "Upow": {13}, "soil1": {14}, "soil2": {15}, "soil3": {16}, "stationID": "00{17}", "device_type": "Z", "firmware_version": {18}, "time": {19}'.format(t, hm, pr, t0, t1, t2, t3, t4, t5, t6, t7, t8, uext, upow, soil1, soil2, soil3, id, ver, ts)+'}')
-
-rs = [grequests.post('https://somnoynadno.ru/controller/send_data', data=params) for params in strs]
+rs = [grequests.post('https://somnoynadno.ru/controller/send_datat', json=params) for params in strs]
 for r in grequests.imap(rs, size=16):
     count += 1
-    if need_inter == 'y' and int_count > inter:
-        time.sleep(1)
-        int_count = 1
     print(count, 'done!')
-    int_count += 1
